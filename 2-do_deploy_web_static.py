c544369facf999421814e3744c391ba1837a1911
Darmimese <darmimesseleseyoum69@gmail.com>
2022-12-24 13:08:16 -0800

    queen

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,47 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
-Fabric script that generates deploys an archive to the web
-servers
+Created on sat Dec 24 14:21:54 2022
+@author: Darmi Seyoum
 """
-from fabric.api import run, put, env
+from fabric.api import local, put, run, env
 from datetime import datetime
-from os.path import isfile, basename
 
-env.hosts = ['3.227.3.77', '3.235.226.255']
+env.user = 'ubuntu'
+env.hosts = ['35.227.35.75', '100.24.37.33']
+
+
+def do_pack():
+    """
+    Targginng project directory into a packages as .tgz
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
-    if not isfile(archive_path):
-        return False
-    filename = basename(archive_path)
+    """Deploy the boxing package tgz file
+    """
     try:
-        no_ext = filename.split(".")[0]
-        put(archive_path, "/tmp/")
-        extract_path = "/data/web_static/releases/{}".format(no_ext)
-        run("mkdir -p {}".format(extract_path))
-        run("tar xzf /tmp/{} -C {}".format(filename, extract_path))
-        run("rm /tmp/{}".format(filename))
-        run("mv {0}/web_static/* {0}/".format(extract_path))
-        run("rm -rf {0}/web_static/".format(extract_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(extract_path))
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
         return True
     except:
         return False
